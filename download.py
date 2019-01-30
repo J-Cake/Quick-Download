4c275ead3122e99aa95f73c9ee3a46846c6966f9
jbis9051 <josh9051@gmail.com>
2019-01-30 08:27:05 -0500

    PARALLEL REQUESTS FINALLY!

--- download.py
+++ download.py
@@ -1,5 +1,6 @@
 import time
-
+import shutil
+import os
 import requests
 import tempfile
 from multiprocessing.pool import ThreadPool
@@ -10,22 +11,21 @@ parts = []
 
 
 class Download:
-    def __init__(self, url, name):
-        print("Download Starting Creation")
+    def __init__(self, url, name, save_location):
+        self.save_location = save_location
+        self.final_temp_file = tempfile.NamedTemporaryFile(delete=False)
         self.url = url
         self.total_length = Download.get_length(url)
         self.name = name
         self.average_percentage = 0
         self.average_index = 0
-        self.throttled_speed = Download.throttled_speed(url)
         self.last_print = 0
         self.parts = []
-        print("Download Finished Creation")
 
     @staticmethod
     def get_length(url):
         response = requests.head(url)
-        return int(response.headers['content-length'])
+        return int(response.headers['Content-Length'])
 
     @staticmethod
     def byte_requests_supported(url):
@@ -34,65 +34,91 @@ class Download:
 
     @staticmethod
     def download_speed():  # https://codereview.stackexchange.com/a/139336/180601
-        url = "http://speedtest.ftp.otenet.gr/files/test100k.db"
+        url = "http://speedtest.ftp.otenet.gr/files/test1Gb.db"
         start = time.time()
-        file = requests.get(url)
-        end = time.time()
+        dl = 0
+        file = requests.get(url, stream=True)
+        time_difference = 0
+        for chunk in file.iter_content(1024):
+            dl += len(chunk)
+            time_difference = time.time() - start
+            if time_difference > 10:
+                break
+        return round(dl / time_difference)
 
-        time_difference = end - start
-        file_size = int(file.headers['Content-Length'])
-        print(file_size / time_difference)
-        return round(file_size / time_difference)
+    @staticmethod
+    def average_download_speed():
+        return int((Download.download_speed()) / 3)
 
     @staticmethod
     def throttled_speed(url):
         start = time.time()
-        file = requests.get(url, headers={'Range': 'bytes=0-1024000'})
-        end = time.time()
-        time_difference = end - start
-        file_size = int(file.headers['Content-Length'])
-        print(file_size / time_difference)
-        return round(file_size / time_difference)
+        dl = 0
+        file = requests.get(url, stream=True)
+        time_difference = 0
+        for chunk in file.iter_content(1024):
+            dl += len(chunk)
+            time_difference = time.time() - start
+            if time_difference > 10:
+                break
+        return round(dl / time_difference)
+
+    @staticmethod
+    def average_throttled_speed(url):
+        return int((Download.throttled_speed(url) + Download.throttled_speed(url) + Download.throttled_speed(url)) / 3)
 
     def average_in(self, percent_done_input, from_part):
+        if self.average_index == 8:
+            self.average_percentage = 0
+            self.average_index = 0
         self.average_percentage = ((self.average_percentage * self.average_index)
                                    + percent_done_input) \
-                                  / self.average_index + 1
+                                  / (self.average_index + 1)
         self.average_index += 1
         if self.average_percentage - self.last_print > 0.01:
-            print("%d%%" % round(self.average_percentage * 100))
+            print(self.average_percentage)
             self.last_print = self.average_percentage
 
     def createParts(self):
-        print("Download Creating Parts")
-        num_of_parts_to_create = int(self.download_speed() / self.throttled_speed) - 1
-        num_of_parts_to_create = 32
-        print(num_of_parts_to_create)
-        last_int = 0
+        num_of_parts_to_create = int(self.average_download_speed() / self.average_throttled_speed(self.url)) - 1
+        num_of_parts_to_create = 4
+        last_int = -1
         for i in range(num_of_parts_to_create):
-            to_byte = int((self.total_length/num_of_parts_to_create) * (i + 1))
-            self.parts.append(Part(self.url, last_int + 1, to_byte))
+            to_byte = int((self.total_length / num_of_parts_to_create) * (i + 1))
+            if i == 1 or i == 0 or 1 == 1:
+                self.parts.append(Part(self.url, last_int + 1, to_byte, self))
             last_int = to_byte
-            print(i)
-        print("Download Done Creating Parts")
         return self
 
     def download_all(self):
         print("Downloading All Parts")
         results = ThreadPool(len(self.parts)).imap_unordered(Download.call_downloader, self.parts)
+        i = 0
         for path in results:
-            print(path)
+            i += 1
+            print("%d of %d complete" % (i, len(self.parts)))
         print("done")
+        return self
 
+    def combineParts(self):
+        with open(self.final_temp_file.name, "wb") as destination:
+            for part in self.parts:
+                shutil.copyfileobj(open(part.file.name, 'rb'),
+                                   destination)  # https://stackoverflow.com/a/1001587/7886229
+            destination.close()
+            return self
+
+    def move_to_final(self):
+        shutil.move(self.final_temp_file.name, self.save_location + "/test.png")
 
     @staticmethod
     def call_downloader(part):
         part.download_bytes()
 
 
-class Part(Download):
+class Part:
 
-    def __init__(self, url, from_byte, to_byte):
+    def __init__(self, url, from_byte, to_byte, parent):
         self.url = url
         self.from_byte = int(from_byte)
         self.to_byte = int(to_byte)
@@ -100,25 +126,24 @@ class Part(Download):
         self.stop_byte = int(to_byte)
         self.file = tempfile.NamedTemporaryFile(delete=False)
         self.percent_done = 0
+        self.parent = parent
 
     def download_bytes(self):  # https://stackoverflow.com/a/16696317/7886229
         r = requests.get(self.url, headers={'Range': 'bytes=%d-%d' % (self.from_byte, self.to_byte)}, stream=True)
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:  # filter out keep-alive new chunks
-                chunk_len = len(chunk)
-                if self.current_byte + chunk_len == self.to_byte:
-                    self.file.write(chunk)
-                    break
-                if self.current_byte + chunk_len > self.to_byte:
-                    self.file.write(chunk[:int(self.to_byte - self.current_byte)])
-                    break
                 self.file.write(chunk)
+                self.file.flush()  # credit to  https://chat.stackoverflow.com/users/5067311/andras-deak
                 self.current_byte += len(chunk)
                 self.percent_done = (self.current_byte - self.from_byte) / (self.to_byte - self.from_byte)
-               # super().average_in(self.percent_done, self)
-                # f.flush() commented by recommendation from J.F.Sebastian
+                self.parent.average_in(self.percent_done, self)
+            # f.flush() commented by recommendation from J.F.Sebastian
 
 
 download = Download(
-    "")
-download.createParts().download_all()
+    "http://127.0.0.1/test.png",
+    "dsfs", '/Users/joshuabrown3/Desktop')
+download.createParts().download_all().combineParts().move_to_final()
+
+# https://1fiag6d.oloadcdn.net/dl/l/uZyoOCpauHw5U_Ru/StR3j2dx_QE/My.Friend.Dahmer.2017.1080p.WEB+DL.DD5.1.H264+FGT.E.mp4?mime=true
+# https://raw.githubusercontent.com/jbis9051/quick_download/master/ping.js
