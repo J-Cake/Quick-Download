b464321f6e4698bbbe3133d4aa84421a8ce4994d
Josh Bronw <josh9051@gmail.com>
2019-04-14 13:39:36 -0400

    windows fix

--- src/components/windowframe.js
+++ src/components/windowframe.js
@@ -18,133 +18,135 @@ window.localStorage.withFrame = window.localStorage.withFrame || 'true';
 let platform = remote.require('os').platform();
 
 if (platform !== "win32" && platform !== "darwin") {
-	platform = "other";
-	if (window.localStorage.hasRelaunched === "false") {
-		window.localStorage.hasRelaunched = "true";
-		window.localStorage.withFrame = "true";
-		_electron.ipcRenderer.send('withFrame');
-	} else {
-		window.localStorage.hasRelaunched = "false";
-		window.localStorage.withFrame = "false";
-	}
+    platform = "other";
+    if (window.localStorage.hasRelaunched === "false") {
+        window.localStorage.hasRelaunched = "true";
+        window.localStorage.withFrame = "true";
+        _electron.ipcRenderer.send('withFrame');
+    } else {
+        window.localStorage.hasRelaunched = "false";
+        window.localStorage.withFrame = "false";
+    }
 } else {
-	if (window.localStorage.withFrame === "true") {
-		window.localStorage.withFrame = "false";
-		window.localStorage.hasReloaded = "false";
-		_electron.ipcRenderer.send('noFrame');
-	}
+    if (window.localStorage.withFrame === "true") {
+        window.localStorage.withFrame = "false";
+        window.localStorage.hasReloaded = "false";
+        _electron.ipcRenderer.send('noFrame');
+    }
 
-	if (_window.frame) {
-		_electron.ipcRenderer.send('noFrame');
-	}
+    if (_window.frame) {
+        _electron.ipcRenderer.send('noFrame');
+    }
 }
 
 export default class WindowFrame extends React.Component {
-	constructor(...args) {
-		super(...args);
+    constructor(...args) {
+        super(...args);
 
-		this.state = {
-			restore: false,
-			showMenu: false
-		}
-	}
+        this.state = {
+            restore: false,
+            showMenu: false
+        }
+    }
 
-	componentDidMount() {
-		Mousetrap.bind('alt', () => this.setState(prev => ({showMenu: !prev.showMenu})));
+    componentDidMount() {
+        Mousetrap.bind('alt', () => this.setState(prev => ({showMenu: !prev.showMenu})));
 
-		if ($(".titlebar")) {
-			$(".min-btn").on("click", e => {
-				_window.minimize();
-			});
-			$(".max-btn").on("click", e => {
-				if (platform !== "darwin")
-					_window.maximize();
-				else
-					_window.setFullScreen(true)
-			});
-			$(".restore-btn").on("click", e => {
-				if (platform !== "darwin")
-					_window.restore();
-				else
-					_window.setFullScreen(false)
-			});
-			$(".close-btn").on("click", e => {
-				_window.close();
-			});
+        if ($(".titlebar")) {
+            $(".min-btn").on("click", e => {
+                _window.minimize();
+            });
+            $(".max-btn").on("click", e => {
+                if (platform !== "darwin")
+                    _window.maximize();
+                else
+                    _window.setFullScreen(true)
+            });
+            $(".restore-btn").on("click", e => {
+                if (platform !== "darwin")
+                    _window.restore();
+                else
+                    _window.setFullScreen(false)
+            });
+            $(".close-btn").on("click", e => {
+                _window.close();
+            });
 
-			this.updateButtons();
+            this.updateButtons();
 
-			_window.on('resize', () => this.updateButtons(), false);
-		}
-	}
+            _window.on('resize', () => this.updateButtons(), false);
+        }
+    }
 
-	updateButtons() {
-		this.setState({restore: _window.isMaximized()});
-	}
+    updateButtons() {
+        this.setState({restore: _window.isMaximized()});
+    }
 
-	render() {
-		if (!_window.frame) {
-			return (
-				<header style={{display: _window.isFullScreen() ? "none" : "block"}} className={`titlebar`}>
-					<div className={`drag-region`}>
-						<div className={`window-title`}>
-							<img src={"./favicon.ico"} className={`icon`} alt={"Quick Downloader"}/>
-							<span>Quick Downloader</span>
-						</div>
+    render() {
+        if (!_window.frame) {
+            return (
+                <header style={{display: _window.isFullScreen() ? "none" : "block"}} className={`titlebar`}>
+                    <div className={`drag-region`}>
+                        <div className={`window-title`}>
+                            <img src={"./favicon.ico"} className={`icon`} alt={"Quick Downloader"}/>
+                            <span>Quick Downloader</span>
+                        </div>
 
-						{platform === "win32" ?
-							<div className="nav_bar_wrapper">
-								<div className="nav_item">File
-									<div className="nav_dropdown">
-										<div>New Download</div>
-									</div>
-								</div>
-								<div className="nav_item">View
-									<div className="nav_dropdown">
-										<div>Theme
-											<div className="nav_dropdown">
-												<div>Light</div>
-												<div>Dark</div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div className="nav_item">Help
-									<div className="nav_dropdown">
-										<div>Contact Developers</div>
-										<div>Learn More</div>
-										<div>Contribute</div>
-										<div>About</div>
-										<div>Docs</div>
-									</div>
-								</div>
-							</div>
-							 : null}
-						<div className={`window-controls ${platform}`}>
-							<div className={`button min-btn ${platform}`}>
-								<span>{platform === "win32" ? "" : ""} </span>
-							</div>
-							<div className={`button max-btn ${platform}`}
-								 style={{display: !this.state.restore ? "inherit" : "none"}}>
-								<span>{platform === "win32" ? "" : ""}</span>
-							</div>
-							<div className={`button restore-btn ${platform}`}
-								 style={{display: this.state.restore ? "inherit" : "none"}}>
-								<span>{platform === "win32" ? "" : ""}</span>
-							</div>
-							<div className={`button close-btn ${platform}`}>
-								<span>{platform === "win32" ? "" : ""}</span>
-							</div>
-						</div>
-					</div>
-				</header>
-			);
-		} else {
-			return (
-				<div>
-					No Header
-				</div>
-			)
-		}
-	}
+                        {platform === "win32" ?
+                            <div className={"nav_bar_container"}>
+                                <div className="nav_bar_wrapper">
+                                    <div className="nav_item">File
+                                        <div className="nav_dropdown">
+                                            <div>New Download</div>
+                                        </div>
+                                    </div>
+                                    <div className="nav_item">View
+                                        <div className="nav_dropdown">
+                                            <div>Theme
+                                                <div className="nav_dropdown">
+                                                    <div>Light</div>
+                                                    <div>Dark</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="nav_item">Help
+                                        <div className="nav_dropdown">
+                                            <div>Contact Developers</div>
+                                            <div>Learn More</div>
+                                            <div>Contribute</div>
+                                            <div>About</div>
+                                            <div>Docs</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            : null}
+                        <div className={`window-controls ${platform}`}>
+                            <div className={`button min-btn ${platform}`}>
+                                <span>{platform === "win32" ? "" : ""} </span>
+                            </div>
+                            <div className={`button max-btn ${platform}`}
+                                 style={{display: !this.state.restore ? "inherit" : "none"}}>
+                                <span>{platform === "win32" ? "" : ""}</span>
+                            </div>
+                            <div className={`button restore-btn ${platform}`}
+                                 style={{display: this.state.restore ? "inherit" : "none"}}>
+                                <span>{platform === "win32" ? "" : ""}</span>
+                            </div>
+                            <div className={`button close-btn ${platform}`}>
+                                <span>{platform === "win32" ? "" : ""}</span>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            );
+        } else {
+            return (
+                <div>
+                    No Header
+                </div>
+            )
+        }
+    }
 }
